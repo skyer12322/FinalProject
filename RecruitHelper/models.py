2f0d8bad77ed320093a255041ffafff455c08c83
Nikita Stoyanovski <nikita.stoyanovski@gmail.com>
2025-03-06 18:56:24 +0300

    Small model fix

--- RecruitHelper/models.py
+++ RecruitHelper/models.py
@@ -14,7 +14,7 @@ class Vacancy(models.Model):
     required_education = models.CharField(max_length=255)   # Требуемое образование
     created_at = models.DateTimeField(auto_now_add=True)    # Когда создано
     updated_at = models.DateTimeField(auto_now=True)        # Когда в последний раз обновили
-    ai_rating = models.PositiveIntegerField(max_value=100) # Рейтинг привлекательности вакансии
+    ai_rating = models.PositiveIntegerField()
 
     def __str__(self):
         return self.title
