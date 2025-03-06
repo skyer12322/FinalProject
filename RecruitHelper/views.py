c1e59346fd411352690e38353738c89c2f4e72b4
syroeshkin <119108430+syroeshkin@users.noreply.github.com>
2025-03-06 23:38:26 +0300

    header fix

--- RecruitHelper/views.py
+++ RecruitHelper/views.py
@@ -4,15 +4,8 @@ from django import forms
 import random
 
 def home(req):
-    vacancies_context = list()
-    if Vacancy.objects.count() > 6:
-        for i in range(6):
-            vacancy_id = random.sample(1, Vacancy.objects.count())
-            vacancy = Vacancy.objects.get(id=vacancy_id)
-            vacancies_context.append(vacancy)
-    else:
-        vacancies_context = Vacancy.objects.all()
-    context = {"vacancies": vacancies_context}
+   
+    context = { }
     return render(req, 'index.html', context)
 
 def login(req):
