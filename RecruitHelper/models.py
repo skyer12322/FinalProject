883349adb335e9e726c8e9c50be83eadff826a8e
Nikita Stoyanovski <nikita.stoyanovski@gmail.com>
2025-03-06 17:30:48 +0300

    Added fields to Vacancy model

--- RecruitHelper/models.py
+++ RecruitHelper/models.py
@@ -5,12 +5,16 @@ import json
 class Vacancy(models.Model):
     ''' Модель для вакансий '''
     title = models.CharField(max_length=255)                # Название вакансии
+    company = models.CharField(max_length=255)              # Название компании
+    location = models.CharField(max_length=255, blank=True) # Географическое положение
+    salary = models.PositiveIntegerField()                  # Зарплата
     description = models.TextField()                        # Описание вакансии
     required_skills = models.JSONField()                    # Требуемые навыки
     required_experience = models.PositiveIntegerField()     # Требуемый опыт (в годах)
     required_education = models.CharField(max_length=255)   # Требуемое образование
     created_at = models.DateTimeField(auto_now_add=True)    # Когда создано
     updated_at = models.DateTimeField(auto_now=True)        # Когда в последний раз обновили
+    ai_rating = models.PositiveIntegerField(max_value=100) # Рейтинг привлекательности вакансии
 
     def __str__(self):
         return self.title
