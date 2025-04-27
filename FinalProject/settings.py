64b872fa1586d1d9a72c1cb377951f8a9a908cad
Nikita Stoyanovski <nikita.stoyanovski@gmail.com>
2025-04-27 14:36:53 +0300

    Debug turned off, added new allowed host

--- FinalProject/settings.py
+++ FinalProject/settings.py
@@ -28,14 +28,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v6*)c!#ny+6w2p$()r7!##z=-+o4fwqu+yv!&j)fp!a!2s&4&e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+MAIN_HOST = os.environ.get('MAIN_HOST')
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
-    CSRF_TRUSTED_ORIGINS = [f'https://{RENDER_EXTERNAL_HOSTNAME}']
+    ALLOWED_HOSTS.append(MAIN_HOST)
+    CSRF_TRUSTED_ORIGINS = [f'https://{RENDER_EXTERNAL_HOSTNAME}', f'https://{MAIN_HOST}']
 
 # Application definition
 
