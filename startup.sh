20b45af8ec135f6b386835048ad03f56b4d6f8fd
Nikita Stoyanovski <nikita.stoyanovski@gmail.com>
2025-05-17 00:17:05 +0300

    startup fix

--- startup.sh
+++ startup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-python manage.py collectstatic --noinput
+export DJANGO_SETTINGS_MODULE="myproject.settings.prod"
+python manage.py collectstatic
 daphne -b 0.0.0.0 -p 10000 FinalProject.asgi:application
